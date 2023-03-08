2a37d4b290063653db2dcbddef8f8c9c94df0ec4
Ben McGraw <ben.mcgraw@gmail.com>
2023-03-08 02:28:51 -0800

    More panels

--- src/app/state-management/in-memory/reducer.ts
+++ src/app/state-management/in-memory/reducer.ts
@@ -8,7 +8,7 @@ export default function reducer(state, action, ...rest) {
     // tools: toolsReducer(state && state.tools, action, ...rest),
     // documents: documentsReducer(state && state.documents, action, ...rest),
     // workspace: workspaceReducer(state && state.workspace, action, ...rest),
-    widgets: widgetsReducer(state && state.widgets, action, ...rest),
+    // widgets: widgetsReducer(state && state.widgets, action, ...rest),
   };
 }
 
